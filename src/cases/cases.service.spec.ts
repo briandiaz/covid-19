4d616e26821d963129d2d227a7788739d47f7987
Brian Díaz <brianediaz7@gmail.com>
2020-03-24 16:16:39 -0400

    Cases: Add cases service.

---
+++ src/cases/cases.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CasesService } from './cases.service';
+
+describe('CasesService', () => {
+  let service: CasesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CasesService],
+    }).compile();
+
+    service = module.get<CasesService>(CasesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
