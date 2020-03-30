bbe5986753816a2f49190bc393658807bcec8227
Brian Díaz <brianediaz7@gmail.com>
2020-03-30 18:51:31 -0400

    Authentication: Add module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { CasesModule } from './cases/cases.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
+import { AuthenticationModule } from './authentication/authentication.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
-    CasesModule
+    CasesModule,
+    AuthenticationModule
   ],
 })
 export class AppModule { }
