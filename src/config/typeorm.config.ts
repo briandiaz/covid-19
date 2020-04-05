2db394a170653e722f7940da48b4f7276420980b
Brian Díaz <brianediaz7@gmail.com>
2020-04-05 01:41:30 -0400

    AWS: Add AWS RDS database environment variables.

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,17 +1,17 @@
 require('dotenv/config');
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-const { DB_PORT, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+const { RDS_PORT, RDS_HOSTNAME, RDS_USERNAME, RDS_PASSWORD, RDS_DB_NAME, SYNC_ORM } = process.env;
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: DB_HOST,
-  port: parseInt(DB_PORT, 10) || 5432,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  host: RDS_HOSTNAME,
+  port: parseInt(RDS_PORT, 10) || 5432,
+  username: RDS_USERNAME,
+  password: RDS_PASSWORD,
+  database: RDS_DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   subscribers: [__dirname + '/../**.module/*-subscriber.{js,ts}'],
   migrations: [__dirname + '/../migrations/*.{js,ts}'],
-  synchronize: true,
+  synchronize: SYNC_ORM === 'true',
 };
